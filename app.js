a9632ccb5608493f5bece94ae2adb7173d4e8a23
Kemal Ahmed <goatandsheep@gmail.com>
2020-10-20 15:56:07 -0400

    package clean-up

--- app.js
+++
@@ -1,3 +0,0 @@
-// TODO: generate magnet links
-//
-
---
+++ index.js
@@ -0,0 +1,26 @@
+class Swarmpack {
+    constructor() {
+        //
+    }
+
+    /**
+     * @typedef {Object} swarmpackCacheItem
+     * @property {String} location backup location in case no WebTorrent support
+     * @property {String} magnet
+     * @property {String} loader
+     */
+
+    /**
+     * Inject value of key into element with 
+     * @param {String} key 
+     * @param {String} id
+     * @param {String} [tagType]
+     */
+    injectKey(key, id, tagType) {
+        if (tagType) {
+            //
+        } else {
+            // TODO: guess using loader
+        }
+    }
+}
